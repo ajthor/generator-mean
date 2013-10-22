c5f1359e2cd63df7875bb7945a8a8250012922ae
Adam <adam@adam-thorpe.local>
2013-10-22 22:19:17 +0200

    Added 'otherwise' route to app template.

--- templates/app.js
+++ templates/app.js
@@ -2,4 +2,8 @@
 	var <%= name %> = angular.module('<%= name %>', [
 		<% for(var i in dependencies) { %>'<%= dependencies[i] %>'<% if(i<dependencies.length-1) { %>,
 		<% }}; %>
-		]);
+		])
+
+		.config(['$routeProvider', function ($routeProvider) {
+			$routeProvider.otherwise({redirectTo: '/'});
+		}]);
