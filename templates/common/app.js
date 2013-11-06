3e0cae95bdf3fd5043f26066f218d1e456ab3061
Adam <adam@adam-thorpe.local>
2013-11-06 23:06:45 +0100

    Added boilerplate to app.js template

--- templates/common/app.js
+++ templates/common/app.js
@@ -1,7 +1,12 @@
 angular.module('<%= name %>', [
 		<% for(var i in dependencies) { %>'<%= dependencies[i] %>'<% if(i<dependencies.length-1) { %>,
 		<% }}; %>
-		])
+		], function($routeProvider, $locationProvider) {
+
+            $routeProvider.otherwise( { redirectTo: '/'} );
+
+            $locationProvider.html5Mode(true);
+        })
 
 		.run(function ($rootScope) {
 			$rootScope.message = "Hello, World!";
