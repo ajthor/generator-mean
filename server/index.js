999c9fa1e576521384f22b6058c36a562f7b72d4
Adam <adam.thorpe.g@gmail.com>
2014-04-29 10:27:52 -0600

    feat: add server template generator

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 'use strict';
+var fs = require('fs');
 var util = require('util');
+var path = require('path');
 var yeoman = require('yeoman-generator');
 
 
-var ServerGenerator = yeoman.generators.NamedBase.extend({
+var ServerGenerator = yeoman.generators.Base.extend({
 	init: function () {},
 
 	directories: function() {
@@ -11,6 +13,22 @@ var ServerGenerator = yeoman.generators.NamedBase.extend({
 		this.dest.mkdir('server/controllers');
 		this.dest.mkdir('server/routes');
 		this.dest.mkdir('server/views');
+	},
+
+	serverFiles: function() {
+		var done = this.async();
+		
+		this.remote('linnovate', 'mean', 'master', function(err, remote) {
+			if (err) {
+				return done(err);
+			}
+
+			// remote.copy('server.js', 'server.js');
+
+			// remote.directory('server', 'server');
+
+			done();
+		});
 	}
 });
 
