e5cee53733145dc8a8d11cfedbaf3e67ca93b0ad
Adam <adam.thorpe.g@gmail.com>
2014-05-03 15:46:32 -0600

    add gruntfile for initialization / move gulpfile

---
+++ app/templates/_Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+	
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		shell: {
+			options: {
+				stdout: true,
+				stderr: true
+			},
+
+			'git-init': {
+				command: [
+					'git init',
+					'git add .',
+					'git commit -a -m \"initial commit\"'
+				].join('&&')
+			}
+		}
+
+	});
+
+	grunt.task.loadNpmTasks('grunt-shell');
+
+	grunt.registerTask('git-init', ['shell:git-init']);
+
+};
+
+
